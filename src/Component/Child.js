aae5a50d1df8895ddd1337abfb035d18ffd115a3
Abdullah-Rajpoot <ar0306654>
2023-10-07 22:55:13 +0500

    https://github.com/Abdullah-Rajpoot/Inputdata.git

---
+++ src/Component/Child.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const Child = (props) => {
+  const [name, setName] = useState("");
+ 
+  function handleSubmit(e) {
+    e.preventDefault(); 
+    props.getData(name);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type='text' 
+          value={name}
+          onChange={(e) => { setName(e.target.value) }}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+
+  );
+}
+
+export default Child;
+
+
+
+
+
